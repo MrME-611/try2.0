102e49b19729cc12aae1e2577ddc66bba312810e
Rahul Arya <47640486+MrME-611@users.noreply.github.com>
2020-07-07 12:54:52 +0530

    Update prob1.py

--- prob1.py
+++ prob1.py
@@ -7,7 +7,7 @@ n=n-1
 for i in range(len(word)):
     n = n+1
     count = 0
-    if (n != len(word)):
+    if (n <= len(word)):
         a =(word[i:n])
         for i in range(len(a)):
             if a[i] in ('a', 'e', 'i', 'o', 'u'):
